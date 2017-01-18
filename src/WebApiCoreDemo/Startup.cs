86ed974f345e15292c4e012707fb1df74586063f
khaled OUESLATI <khaled.oueslati@zags.com>
2017-01-18 15:57:47 +0100

    Update solution to use IDE Visual Studio Core

--- src/WebApiCoreDemo/Startup.cs
+++ src/WebApiCoreDemo/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebApiCoreDemo.Models;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using WebApiCoreDemo.Repository;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using NetEscapades.AspNetCore.SecurityHeaders;
